b40bf052af87d144ef29ada0eea35b131d348716
21011332 <21011332ym@tama.ac.jp>
2014-12-03 15:35:23 +0900

    リュカ完成２

--- Ryuka.java
+++ Ryuka.java
@@ -7,8 +7,9 @@ class ryuka {
 		BigInteger[] hai = new BigInteger[101];
 	
 	hai[0] = new BigInteger ("2");
- 	
  	hai[1] = new BigInteger("1");
+	ryuka = ryuka.add(hai[1]);
+	
 	
 	for (int i=2;i<=100;i++){
  	     hai[i] = hai[i-1].add(hai[i-2]);
