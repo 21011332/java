4a45950e61208fe0414e60ed8a925472171e662d
21011332 <21011332ym@tama.ac.jp>
2014-12-10 16:05:21 +0900

    file.java

--- file.java
+++ file.java
@@ -1,23 +1,30 @@
 import java.util.Random;
 import java.io.*;
+
 class file {
  
      public static void main(String[] args){
         Random b = new Random();
 		
-		int a = b.nextInt(101);
-		System.out.println(a);
+		
 		try{
-		File file = new File("tensu.txt");
-		FileWriter fw = new FileWriter(file); 
-		BufferedWriter bw = new BufferedWriter(fw);
-		PrintWriter pw = new PrintWriter(bw);
-		pw.println(a);
-		pw.close();
+			File file = new File("tensu.csv");
+			FileWriter fw = new FileWriter(file); 
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter pw = new PrintWriter(bw);
+			
+				for (int a = 1 ;a<=100; a++){
+				
+				a= b.nextInt(101);
+				pw.print(a+",");
+				}
+				
+			
+			
+			pw.close();
 		}catch(Exception e)
 		{
 		System.out.println(e.toString());
  		}
 	}
-	
 }	
